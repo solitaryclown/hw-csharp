7f67f948344920347f0670c32aebc551f54c89b2
huangbei <2422346850@qq.com>
2022-06-08 19:15:52 +0800

    修改Program.cs

--- testCS/Program.cs
+++ testCS/Program.cs
@@ -1,4 +1,18 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
-Console.Write(Console.ReadLine());
 
+//Console.WriteLine(Console.ReadLine());
+
+Console.WriteLine("这是一个C#程序");
+int a = 10;
+Console.WriteLine(a);
+int b = Int32.Parse(Console.ReadLine());
+Console.WriteLine(b);
+if (b >= 0)
+{
+    Console.WriteLine("b是一个非负整数");
+}
+else
+{
+    Console.WriteLine("b是一个负数");
+}
